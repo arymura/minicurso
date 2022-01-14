838611649d6b2cb3c585a60ef22fabb4c81c2663
arymura <arymura@gmail.com>
2022-01-14 19:05:24 -0300

    Capturar elementos html manipulaveis

---
+++ Script.js
@@ -0,0 +1,8 @@
+const imagem = document.querySelector('img');
+const botao = document.querySelector('button');
+const nomeDoPersonagem = document.querySelector('#nome')
+const especie = document.querySelector('#Especie');
+const status = document.querySelector('status');
+
+
+
